f1c8496b202368c995f52295a5ffdba5925baa8e
ATMarcks <alex.marck@colorado.edu>
2018-10-19 21:19:50 -0600

    Finish tumblr

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-let express = require('express')
-let history = require('connect-history-api-fallback')
-let path = require('path')
+let express = require('express');
+let history = require('connect-history-api-fallback');
+let path = require('path');
+let https = require('https');
 let Sentiment = require('sentiment');
-let scrapeTwitter = require('scrape-twitter')
-let port = process.env.PORT || 80
+let scrapeTwitter = require('scrape-twitter');
+let port = process.env.PORT || 80;
 
-let app = express()
+let app = express();
 let sentiment = new Sentiment();
 
 // Allow CORS if in development
@@ -71,7 +72,7 @@ app.get('/api/gettwitterdata', (req, res) => {
 
     topTweets.on('data', (chunk) => {
         unreadTweetsToShow.push({
-            'time': chunk.time,
+            'time': new Date(chunk.time).getTime() / 1000,
             'text': chunk.text,
             'retweets': chunk.retweetCount,
             'favorites': chunk.favoriteCount
@@ -92,6 +93,7 @@ app.get('/api/gettwitterdata', (req, res) => {
             });
             unreadTweetsToShow = unreadTweetsToShow.sort(() => .5 - Math.random()).slice(0,9);
             res.status(200).send({
+                'success': true,
                 'semanticScore': semanticAnalysisTwitter.scoreSum/ semanticAnalysisTwitter.samples,
                 'tweets': unreadTweetsToShow
             })
@@ -99,4 +101,51 @@ app.get('/api/gettwitterdata', (req, res) => {
     });
 });
 
-app.listen(port)
+app.get('/api/gettumblrdata', (req, res) => {
+    const tag = req.query.tag;
+    const tumblrPosts = [];
+
+    let semanticAnalysisTumblr = {
+        'samples': 0,
+        'scoreSum': 0
+    };
+
+    //consumer key: sRwdivS37Z30JHMXjKezO0Y5awC92OX9TF2sRHAn1JET1H4DiC
+    //key: w1ZXh8PpXMYGoZQplSLWfG58U22PQkzKOf63mfVEcyoKWa7Ci8
+    https.get('https://api.tumblr.com/v2/tagged?tag=' + tag + '&filter=text&api_key=sRwdivS37Z30JHMXjKezO0Y5awC92OX9TF2sRHAn1JET1H4DiC', (resp) => {
+        resp.setEncoding('utf8');
+        let resJson = '';
+
+        resp.on('data', body => {
+            resJson += body;
+        });
+
+        resp.on('end', () => {
+            let resObj = JSON.parse(resJson);
+            resObj.response.forEach(post => {
+                if (post.body) {
+                    tumblrPosts.push({
+                        'time': post.timestamp,
+                        'text': post.body
+                    });
+                    const senRes = sentiment.analyze(post.summary);
+                    semanticAnalysisTumblr.samples += 1;
+                    semanticAnalysisTumblr.scoreSum += (senRes.score + 5) * 10;
+                }
+            });
+
+            res.status(200).send({
+                'success': true,
+                'semanticScore': semanticAnalysisTumblr.scoreSum/ semanticAnalysisTumblr.samples,
+                'blogs': tumblrPosts
+            });
+        });
+    }).on('error', (err) => {
+        res.status(200).send({
+            'success': false,
+            'error': 'Could not connect to Tumblr'
+        });
+    });
+});
+
+app.listen(port);
